5492037741c2c6f2647cc54be571cd01464e4510
DongHun Kim <31065684+d-h-k@users.noreply.github.com>
2020-01-24 16:08:55 +0900

    Update Q532.cpp

--- 코테준비/1_book_follow/Q532.cpp
+++ 코테준비/1_book_follow/Q532.cpp
@@ -20,5 +20,21 @@ int main() {
     scanf("%d", &T);
     for(int test_case =1 ; test_case <=T ; test_case++ ) {
         scanf("%d %d ", &P, &N);
+        for(int i=1 ; i<=N ; i++ ) {
+            scanf("%d %d",&M[i].k, &M[i].B);
+            M[i].idx = i;
+        }
+        
+        sort(M+1 , M+N+1, comp);
+        is_Answer=1;
+        
+        for(int i=0 ; i<=N ; i++ ) {
+            if(M[i].K < P) {
+                P = P + M[i].B;
+            }
+            else{
+                is_Answer= -1;
+                break;)
+        }
     }
 }
