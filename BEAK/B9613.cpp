d55b88cf2bdb392fb7a02ec36d8fb81515f7bcf9
DHKim <ultraraptor@naver.com>
2020-02-02 12:11:17 +0900

    BEAK add

---
+++ BEAK/B9613.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+
+int main() {
+	int TC;
+	scanf("%d",&TC);
+	for(int test_case=0 ; test_case<TC ; test_case++) {
+		int num,;
+		scanf("%d",&num);
+		for(int i=0; i<num ; i++) {
+			scanf("%d",&)
+		}
+	}
+	return 0;
+}
