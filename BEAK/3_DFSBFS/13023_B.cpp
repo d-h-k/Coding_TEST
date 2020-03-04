236957961a7b798f6110c36c110786a8e52814c8
DHKim <ultraraptor@naver.com>
2020-03-04 11:06:16 +0900

    DFS BFS

---
+++ BEAK/3_DFSBFS/13023_B.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool a[2000][2000];
+vector<int> g[2000];
+vector<pair<int,int>> edges;
+
+int n, m;
+
+int main(void) {
+    //printf("Hello world!");
+	scanf("%d %d",&n,&m);
+	
+	for(int i=0; i<m ; i++) {
+		int from, to;
+		scanf("%d %d",&from,&to);
+		edges.push_back({from,to});
+		edges.push_back({to,from});
+		a[from][to] = a[to][from] = true;
+		g[from].push_back(to);
+		g[to].push_back(from);
+	}
+	
+	m*=2;
+	for(int i=0 ; i<m ; i++) {
+		for(int j = 0 ; j<m ; j++) {
+			//A -> B
+			int A = edges[i].first;
+			int B = edges[i].second;
+			
+			//C -> D
+			int C = edges[i].first;
+			int D = edges[i].second;
+			
+			if(A==B || A==C || A==D || B==C || B==D || C==D ) {
+				continue;
+			}
+			
+			if(!a[B][C]) {
+				continue;
+			}
+			
+			for(int E : g[D]){
+                if (A == E || B == E || C == E || D == E) {
+                    continue;
+                }
+                cout << 1 << '\n';
+                return 0;
+            }
+        }
+    }
+    cout << 0 << '\n';
+    return 0;
+}
+
