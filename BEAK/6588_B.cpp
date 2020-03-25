235a08f777da419b329299c6fbe5ab258a4f4633
DHKim <ultraraptor@naver.com>
2020-03-25 14:03:03 +0900

    update 0325r

--- BEAK/6588_B.cpp
+++ BEAK/6588_B.cpp
@@ -1,35 +1,97 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-#include<iostream>
+#include <iostream>
+#include <cstring>
 
 using namespace std;
-int prime[1000000];
-int pn=0;
-bool check[1000001]={0};//true=Not prime, false=prime;
+
+int prime[50001] = {0,};//prime svae arr
+int pn=0; // number of prime
+
+bool np_check[100001] = {false,};//false = YES prime ,  true = NO prime
+
 const int DEBUG = 1;
-int is_prime(int num);
+
+int  is_prime(int num) {
+	return !np_check[num];
+}
+
 int main(int argc, char** argv)
 {
-	int n=1000000,gcn;
+	int n=0,gcn;
 	int a, b;
+	int gol_num = 0;
+	//init
+	
+	while(1) {
+		//init
+		pn=0;
+		memset(np_check,false,sizeof(np_check));
+		memset(prime,0,sizeof(prime));
+		//
+		scanf("%d",&gol_num);
+		if(gol_num==0) {
+			exit(1);
+		}
+		else {
+			//1. get prime table
+			for(int i=2 ; i*i<=gol_num ; i++) {
+				if( np_check[i] == false ) {
+					prime[pn]=i;
+					pn++;
+					
+					for(int j=2 ; j*(i)<=gol_num ; j++) {
+						np_check[i] = true;
+						
+					}
+				}
+				else {
+				}		
+			}
+			
+			//2. use pritbl, brute-Force search
+			for(int i=0 ; i<pn ; i++) {
+				int temp = gol_num - prime[i];
+				
+				if(temp == prime[i]) {
+						printf("%d = %d + %d\n",gol_num,temp,prime[i]);
+						break;
+					}
+					else if(temp < prime[i]) {
+						printf("Goldbach's conjecture is wrong.");
+						break;
+					}
+					else {
+						
+					}
+				
+			}
+			
+			/*
+			if(DEBUG) {
+				for(int i=0 ; i<pn ; i++) {
+					printf("%d ",prime[i]);	
+				}
+			}*/
+			
+			
+		}	
+	}
 	
 	
-	for(int i=2 ; i*i<=n ; i++) {
+	/*for(int i = 2 ; i*i <= n ; i++) {
 		if(check[i] == false) {
-			prime[pn++]=i;
-			for(int j=i*2 ; j<=n ; j+=i) {
+			prime[pn++] = i;
+			for(int j = i*2 ; j <= n ; j+=i) {
 				check[j]=true;
 			}
 		}
 	}
+	*/
 
+	
+	
+	
 	/*
-	if(DEBUG) {
-		for(int i=0 ; i<pn ; i++) {
-			printf("%d ",prime[i]);	
-		}
-	}*/
+	///
 	do{
 		scanf("%d",&gcn);
 		if(gcn==0) {
@@ -57,10 +119,9 @@ int main(int argc, char** argv)
 		printf("%d %d",a,b);
 			
 	}while(1);
+	*/
+	
 	
 	return 0;//
 }
 
-int  is_prime(int num) {
-	return !check[num];
-}
