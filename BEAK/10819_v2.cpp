8b78e423c006a35f7e18282145951eae5167a4e9
DHKim <ultraraptor@naver.com>
2020-03-27 10:55:23 +0900

    miro slove

--- BEAK/10819_v2.cpp
+++ BEAK/10819_v2.cpp
@@ -6,31 +6,22 @@
 using namespace std;
 
 //int diff_cal();
-//vector<int> v;
+vector<int> v;
 vector<int> diff;
-int v[10];
+//int v[10];
 int len_v;
 const int DEBUG = 0;
 
 int main(int argc, char** argv)
 {
-	int num,maxinum=0;
-	FILE * fp;
-	
-//	fp = freopen("./testdata/10819_B.txt","r",stdin);
-//	if(DEBUG && (fp==0)) {
-//		puts("NO FILE");
-//		exit(1);		
-//	}
-	
+	int num,ans=0;
 	scanf("%d", &num);
-	//
-	//len_v=num;
+	
 	for(int i=0 ; i<num ; i++) {
 		int temp;
 		scanf("%d",&temp);
-		v[i] = temp;
-		//v.push_back(temp);
+		//v[i] = temp;
+		v.push_back(temp);
 	}
 	
 	sort(&v[0], &v[0]+num);	
@@ -43,24 +34,26 @@ int main(int argc, char** argv)
 	}
 	
 	
-	int temp=0, sum=0;
-	maxinum=0;
+	ans=0;
 	do{
-		temp=0;sum=0;
+		int temp=0, sum=0;
+	
 		for(int i=0 ; i<num-1 ; i++) {
 			
-			temp = (v[i]-v[i+1]);	
-			sum += abs(temp);	
-			
+			temp = (v[i]-v[i+1]);
+			sum += abs(temp);
 		}
-		maxinum=max(maxinum,sum);
-	
-	
-	//printf("sun : %d, maxinum : %d \n",sum,maxinum);
+		
+		if(ans<sum) {
+			ans=sum;
+		}
+		
+		
 	}while(next_permutation(&v[0],&v[0]+num));
 	
 	
 	
-	printf("%d\n",maxinum);
+	printf("%d\n",ans);
 	return 0;//
 }
+
